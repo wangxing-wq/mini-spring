8c81a1c8c2e11f286a7eeca2df7f1a476cb535b6
weishengwx <2234346829@qq.com>
2023-01-04 20:52:27 +0800

    04 补充没有默认构造函数的缺陷,使用策略模式动态选用实现方案

---
+++ src/main/java/com/wx/springframework/context/PropertyValues.java
@@ -0,0 +1,8 @@
+package com.wx.springframework.context;
+
+/**
+ * @author 22343
+ * @version 1.0
+ */
+public class PropertyValues {
+}
---
+++ src/main/java/com/wx/springframework/context/config/BeanReference.java
@@ -0,0 +1,8 @@
+package com.wx.springframework.context.config;
+
+/**
+ * @author 22343
+ * @version 1.0
+ */
+public interface BeanReference {
+}
